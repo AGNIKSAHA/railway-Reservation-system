5d5283b60790df80056803673303662dc822aee8
agniksaha <agniksaha9953@gmail.com>
2024-11-14 02:46:48 +0530

    a1

--- railwayResevationSystem/src/App.jsx
+++ railwayResevationSystem/src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from "./component/Navbar";
 // import logo from "./images/logo.png";
 import "./App.css";
 import NavBbar from "./component/NavBbar";
-import MidPart from "./MidPart";
+import MidPart from "./component/MidPart";
 import Footer from "./component/Footer";
-import CurrentTime from "./CurrentTime";
+import CurrentTime from "./component/CurrentTime";
 
 
 function App() {
